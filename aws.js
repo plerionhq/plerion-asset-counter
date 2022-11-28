e25a90f04a3edc8df816598a3a86214638122fd2
Edmund Cong <edmund.cong@plerion.com>
2022-11-28 12:48:17 +1100

    adding IAM

--- aws.js
+++ aws.js
@@ -15,10 +15,11 @@ import {
   DescribeSnapshotsCommand,
   paginateDescribeInstances,
 } from "@aws-sdk/client-ec2";
+import { GlacierClient, paginateListVaults } from "@aws-sdk/client-glacier";
 import {
-  GlacierClient,
-  paginateListVaults,
-} from '@aws-sdk/client-glacier';
+  IAMClient,
+  paginateGetAccountAuthorizationDetails,
+} from "@aws-sdk/client-iam";
 
 const MAPPING = { total: 0 };
 
@@ -26,7 +27,74 @@ const DDB_SCALABLE_TARGET = "AWS::DynamoDB::ScalableTarget";
 const EC2_INSTANCE = "AWS::EC2::Instance";
 const EC2_DEFAULT_EBS_ENCRYPTION = "AWS::EC2::DefaultEBSEncryption";
 const EC2_SNAPSHOT = "AWS::EC2::Snapshot";
-const GLACIER_VAULT = "AWS::Glacier::Vault"
+const GLACIER_VAULT = "AWS::Glacier::Vault";
+const IAM_GROUP = "AWS::IAM::Group";
+const IAM_ROLE = "AWS::IAM::Role";
+const IAM_USER = "AWS::IAM::User";
+const IAM_POLICY = "AWS::IAM::Policy";
+
+const queryIAMAccountAuthorization = async (
+  serviceName,
+  resourceType,
+  region
+) => {
+  const users = [];
+  const groups = [];
+  const roles = [];
+  const managedPolicies = [];
+  console.log(JSON.stringify(MAPPING[serviceName]));
+  if (
+    MAPPING[serviceName] &&
+    MAPPING[serviceName][resourceType] !== undefined
+  ) {
+    return;
+  }
+  const client = new IAMClient({ region });
+  for await (const page of paginateGetAccountAuthorizationDetails(
+    { client },
+    {}
+  )) {
+    if (page.UserDetailList && page.UserDetailList.length) {
+      users.push(...(page.UserDetailList || []));
+      updateResourceTypeCounter(
+        serviceName,
+        IAM_USER,
+        page.UserDetailList.length
+      );
+    } else {
+      updateResourceTypeCounter(serviceName, IAM_USER, 0);
+    }
+    if (page.GroupDetailList && page.GroupDetailList.length) {
+      groups.push(...(page.GroupDetailList || []));
+      updateResourceTypeCounter(
+        serviceName,
+        IAM_GROUP,
+        page.GroupDetailList.length
+      );
+    } else {
+      updateResourceTypeCounter(serviceName, IAM_GROUP, 0);
+    }
+    if (page.RoleDetailList && page.RoleDetailList.length) {
+      roles.push(...(page.RoleDetailList || []));
+      updateResourceTypeCounter(
+        serviceName,
+        IAM_ROLE,
+        page.RoleDetailList.length
+      );
+    } else {
+      updateResourceTypeCounter(serviceName, IAM_ROLE, 0);
+    }
+    if (page.Policies && page.Policies.length) {
+      managedPolicies.push(...(page.Policies || []));
+      updateResourceTypeCounter(serviceName, IAM_POLICY, page.Policies.length);
+    } else {
+      updateResourceTypeCounter(serviceName, IAM_POLICY, 0);
+    }
+  }
+  const total =
+    users.length + groups.length + roles.length + managedPolicies.length;
+  MAPPING.total += total;
+};
 
 const queryGlacierVault = async (serviceName, resourceType, region) => {
   let total = 0;
@@ -231,7 +299,7 @@ export const queryAWS = async (parsedService, parsedResourceType) => {
         continue;
       }
       for (const region of regions) {
-        console.log(`Checking ${serviceName} on region ${region}`);
+        console.log(`Checking ${resourceType} on region ${region}`);
         try {
           switch (resourceType) {
             case DDB_SCALABLE_TARGET:
@@ -252,6 +320,16 @@ export const queryAWS = async (parsedService, parsedResourceType) => {
               break;
             case GLACIER_VAULT:
               await queryGlacierVault(serviceName, resourceType, region);
+              break;
+            case IAM_GROUP:
+            case IAM_ROLE:
+            case IAM_USER:
+            case IAM_POLICY:
+              await queryIAMAccountAuthorization(
+                serviceName,
+                resourceType,
+                region
+              );
               break;
             default:
               await queryDependencies(serviceName, resourceType, region);
