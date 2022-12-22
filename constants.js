bcd8ddc9de168aa0ab63312de941d1435564d924
Ed <edmund.cong@plerion.com>
2022-12-22 15:49:41 +1100

    feat: CSPM-1037 Adding asset unit counter to Plerion Asset Counter (#1)
    * feat: CSPM-1037 Adding asset unit counter
    
    * feat: CSPM-1037 Adding CWPP counts to CSPM too
    
    * fix: CSPM-1037 Adding null check
    
    * fixing up ECS callout

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const UNIT_MAPPING = {
+  AWS: {
+    // default is 1 unit, unless CWPP
+    EC2: {
+      "AWS::EC2::Instance": 10,
+    },
+    Lambda: { "AWS::Lambda::Function": 10 },
+    ECS: { "AWS::ECS::Cluster": 10 },
+  },
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { writeFile } from "fs/promises";
 import { Command } from "commander";
 import { queryAWS } from "./AWS/aws.js";
 
+import { UNIT_MAPPING } from "./constants.js";
+
 const program = new Command();
 program
   .option("-p, --provider <provider>")
@@ -14,6 +16,28 @@ program.parse();
 const options = program.opts();
 const AWS = "AWS";
 
+const calculateUnits = (result, provider) => {
+  const providerUnits = UNIT_MAPPING[provider];
+  let cspmUnits = 0;
+  let cwppUnits = 0;
+  Object.keys(result).forEach((serviceName) => {
+    if (serviceName === "total") {
+      return;
+    }
+    const serviceResourceTypesCounts = result[serviceName];
+    const serviceUnits = providerUnits[serviceName];
+    Object.keys(serviceResourceTypesCounts).forEach((resourceTypeCount) => {
+      if (serviceUnits && serviceUnits[resourceTypeCount] !== undefined) {
+        cwppUnits +=
+          serviceUnits[resourceTypeCount] *
+          serviceResourceTypesCounts[resourceTypeCount];
+      }
+      cspmUnits += serviceResourceTypesCounts[resourceTypeCount];
+    });
+  });
+  return { CSPM: cspmUnits, CWPP: cwppUnits };
+};
+
 (async () => {
   const { provider, service, resource: resourceType, verbose } = options;
   let result;
@@ -22,8 +46,16 @@ const AWS = "AWS";
       result = await queryAWS(service, resourceType);
       break;
   }
+  const { CSPM: CSPM_UNITS, CWPP: CWPP_UNITS } = calculateUnits(
+    result,
+    provider
+  );
+  result["CSPM_UNITS"] = CSPM_UNITS;
+  result["CWPP_UNITS"] = CWPP_UNITS;
+  console.log("-----------------------------------");
   console.log("OUTPUT: ");
   console.log(result, null, 2);
+  console.log("-----------------------------------");
   if (verbose) {
     await writeFile(`${provider}-output.json`, JSON.stringify(result));
   } else {
@@ -32,4 +64,8 @@ const AWS = "AWS";
       JSON.stringify({ total: result.total })
     );
   }
+  console.log("CSPM/CIEM Asset Units: " + CSPM_UNITS);
+  console.log("CWPP Asset Units: " + CWPP_UNITS);
+  console.log("Total Asset Units: " + (CWPP_UNITS + CSPM_UNITS));
+  console.log("-----------------------------------");
 })();
