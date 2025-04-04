baf9dfe850128e302fa77a18c05c5479a8501408
plerionedmund <136040881+plerionedmund@users.noreply.github.com>
2025-04-04 14:22:41 +1100

    imp: PL-2510 Increasing count of ECR ContainerImage (#50)
    * imp: PL-2510 Increasing count of ECR ContaierImage
    
    * removing filtering logic
    
    * getting 100 images
    
    * limiting container images to max 10 per repo

--- AWS/collectors/custom/ECR/ContainerImage.js
+++ AWS/collectors/custom/ECR/ContainerImage.js
@@ -5,6 +5,8 @@ import {
 } from "@aws-sdk/client-ecr";
 import { updateResourceTypeCounter } from "../../../utils/index.js";
 
+const CONTAINER_IMAGE_LIMIT = 10;
+
 export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
   const client = new ECRClient({ region });
   let total = 0;
@@ -31,7 +33,10 @@ export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
     const images = [];
     for await (const page of paginateDescribeImages(
       { client },
-      { repositoryName: repository.repositoryName, maxResults: 1000 },
+      {
+        repositoryName: repository.repositoryName,
+        maxResults: CONTAINER_IMAGE_LIMIT,
+      },
     )) {
       if (page.imageDetails) {
         for (const image of page.imageDetails) {
@@ -42,59 +47,24 @@ export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
           });
         }
       }
+      if (images.length >= CONTAINER_IMAGE_LIMIT) {
+        break;
+      }
     }
 
     if (!images.length) {
       continue;
     }
 
-    // sort by latest pushed images
-    const latestPushedImages = images.sort((a, b) => {
-      const aTime = a.imagePushedAt
-        ? new Date(a.imagePushedAt).getTime()
-        : -Infinity;
-      const bTime = b.imagePushedAt
-        ? new Date(b.imagePushedAt).getTime()
-        : -Infinity;
-      return bTime - aTime;
-    });
+    const imagesToCollect = images.slice(0, CONTAINER_IMAGE_LIMIT);
+    const lengthOfImages = imagesToCollect.length;
 
-    const latestPushedImage = latestPushedImages[0];
-    // get the two latest pulled images by date
-    const latestPullImages = images
-      .filter(
-        (image) =>
-          image.ImageId !== latestPushedImage.ImageId &&
-          !!image.lastRecordedPullTime,
-      )
-      .sort((a, b) => {
-        const aTime = a.lastRecordedPullTime
-          ? new Date(a.lastRecordedPullTime).getTime()
-          : -Infinity;
-        const bTime = b.lastRecordedPullTime
-          ? new Date(b.lastRecordedPullTime).getTime()
-          : -Infinity;
-        return bTime - aTime;
-      })
-      .slice(0, 2);
-    const addedImageIds = new Set([
-      latestPushedImage.ImageId,
-      ...latestPullImages.map(({ ImageId }) => ImageId),
-    ]);
-    const remainingIds = 3 - addedImageIds.size;
-    const repoCount = [
-      latestPushedImage,
-      ...latestPullImages,
-      ...latestPushedImages
-        .filter((image) => !addedImageIds.has(image.ImageId))
-        .slice(0, remainingIds),
-    ].length;
-    total += repoCount;
+    total += lengthOfImages;
     updateResourceTypeCounter(
       AWS_MAPPING,
       serviceName,
       resourceType,
-      repoCount,
+      lengthOfImages,
     );
   }
   AWS_MAPPING.total += total;
