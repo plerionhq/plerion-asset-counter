d666d6c890000a85ce82e760959d8f8c42ed0943
Senal Kumarage <senal.kumarage@plerion.com>
2025-08-28 13:34:22 +1000

    feat: PL-3793 count AWS SSM parameters
    Signed-off-by: Senal Kumarage <senal.kumarage@plerion.com>

---
+++ AWS/collectors/custom/SSM/Parameter.js
@@ -0,0 +1,24 @@
+import { paginateDescribeParameters, SSMClient } from "@aws-sdk/client-ssm";
+import { updateResourceTypeCounter } from "../../../utils/index.js";
+
+export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
+  let resources = [];
+  const client = new SSMClient({ region });
+  for await (const page of paginateDescribeParameters(
+    { client },
+    {
+      MaxResults: 50,
+    },
+  )) {
+    const { Parameters: parameters } = page;
+    resources.push(...(parameters || []));
+  }
+  const resourceCount = resources.length;
+  updateResourceTypeCounter(
+    AWS_MAPPING,
+    serviceName,
+    resourceType,
+    resourceCount,
+  );
+  AWS_MAPPING.total += resourceCount;
+};
