50c368203bc0ae40c4d29a301705e38477c841f7
Manish Gopal Singh <manish.singh@plerion.com>
2026-02-25 16:32:04 +0545

    fix: MI-71 remove supported region

--- AWS/collectors/custom/Bedrock/ImportedModel.js
+++ AWS/collectors/custom/Bedrock/ImportedModel.js
@@ -4,18 +4,7 @@ import {
 } from "@aws-sdk/client-bedrock";
 import { updateResourceTypeCounter } from "../../../utils/index.js";
 
-const SUPPORTED_REGIONS = [
-  "eu-central-1",
-  "us-east-1",
-  "us-east-2",
-  "us-west-2",
-];
-
 export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
-  if (!SUPPORTED_REGIONS.includes(region)) {
-    updateResourceTypeCounter(AWS_MAPPING, serviceName, resourceType, 0);
-    return;
-  }
   let resources = [];
   const client = new BedrockClient({ region });
   for await (const page of paginateListImportedModels({ client }, {})) {
--- AWS/collectors/custom/Bedrock/ModelImportJob.js
+++ AWS/collectors/custom/Bedrock/ModelImportJob.js
@@ -5,23 +5,12 @@ import {
 import { ModelImportJobStatus } from "@aws-sdk/client-bedrock";
 import { updateResourceTypeCounter } from "../../../utils/index.js";
 
-const SUPPORTED_REGIONS = [
-  "eu-central-1",
-  "us-east-1",
-  "us-east-2",
-  "us-west-2",
-];
-
 const INCLUDED_JOB_STATUSES = [
   ModelImportJobStatus.COMPLETED,
   ModelImportJobStatus.IN_PROGRESS,
 ];
 
 export const query = async (AWS_MAPPING, serviceName, resourceType, region) => {
-  if (!SUPPORTED_REGIONS.includes(region)) {
-    updateResourceTypeCounter(AWS_MAPPING, serviceName, resourceType, 0);
-    return;
-  }
   let resources = [];
   const client = new BedrockClient({ region });
   for await (const { modelImportJobSummaries } of paginateListModelImportJobs(
